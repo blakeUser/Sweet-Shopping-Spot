70bb072e694f12b4a0c76d02405d324b76bac39a
jintian4 <jintian4@illinois.edu>
2023-01-08 14:34:37 -0600

    Jan 8th

--- app/src/main/java/com/example/udemyshopping/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/udemyshopping/activity/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.example.udemyshopping.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
@@ -11,7 +12,10 @@ import com.example.udemyshopping.databinding.ActivityLoginBinding
 import com.example.udemyshopping.databinding.ActivityRegisterBinding
 
 @Suppress("DEPRECATION")
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -19,6 +23,8 @@ class RegisterActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        binding =  ActivityRegisterBinding.inflate(layoutInflater)
 
         // START
         setupActionBar()
@@ -30,25 +36,69 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        val register_button = findViewById<TextView>(R.id.btn_register)
+        register_button.setOnClickListener {
+            // Launch the register screen when the user clicks on the text.
+            validateRegisterDetails()
+        }
+
     }
 
-    private lateinit var binding: ActivityRegisterBinding
+
 
     private fun setupActionBar() {
-
-        binding =  ActivityRegisterBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbarRegisterActivity)
-
         val actionBar = supportActionBar
-
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
         }
-
         binding.toolbarRegisterActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun validateRegisterDetails(): Boolean {
+//        binding =  ActivityRegisterBinding.inflate(layoutInflater)
+        return when {
+            TextUtils.isEmpty(binding.etFirstName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etLastName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etConfirmPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            binding.etPassword.text.toString().trim { it <= ' ' } != binding.etPassword.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+            }
+            !binding.cbTermsAndCondition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("感谢使用！", false)
+                true
+            }
+        }
     }
 }
